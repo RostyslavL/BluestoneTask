af1cf6a866eb62a180d6bd58469d2cdf854b9095
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-21 17:20:28 +0200

    feat: add ProductScreen basic markup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
@@ -13,6 +14,7 @@ function App() {
       <main className="py-3">
           <Container>
           <Route path='/' component={HomeScreen} exact></Route>
+          <Route path='/product/:id' component={ProductScreen}></Route>
           </Container>
         </main>
       <Footer/>
