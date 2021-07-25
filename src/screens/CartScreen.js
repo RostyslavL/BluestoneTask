20747d6ede3e273242cdc2875ecf4c6889863d40
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-25 09:15:54 +0200

    feat: create a CartScreen  basic markup

---
+++ src/screens/CartScreen.js
@@ -0,0 +1,23 @@
+import React , {useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector} from 'react-redux'
+import Message from '../components/Message'
+import { 
+    Row, 
+    Col, 
+    ListGroup, 
+    Image, 
+    Form, 
+    Button, 
+    Card 
+} from 'react-bootstrap'
+
+const CartScreen = () => {
+    return(
+        <>
+            Cart
+        </>
+    )
+}
+
+export default CartScreen
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,57 +1,50 @@
-import React , {useState, useEffect} from 'react';
+import React , { useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 // import Data from '../data/data.json';
 import { Row , Col} from 'react-bootstrap';
 import Product from '../components/Product';
-// import Loader from '../components/Loader';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
+    
+    const productList = useSelector(state => state.productList)
+    const {loading, error, products } = productList
 
-    const [data,setData] = useState([]);
+    const dispatch = useDispatch()
 
-    const dataUrl =  'data.json'
-
-    const getData = () =>{
-        fetch(dataUrl
-        ,{
-          headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-        })
-          .then(function(response){
-            // console.log(response)
-            return response.json()
-        })
-          .then(function(data) {
-            // console.log(data)
-            setData(data)
-        });
-    }
     useEffect(()=>{
-      getData()
-    },[])
+        dispatch(listProducts())
+    },[dispatch])
 
-    console.log(data);
+    // console.log(data);
 
     return (
         <>
-            <Row>
-                {data && data.length > 0 &&  data.map((product)=>(
-                    
-                    <Col
-                        key={product._id}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        xlg={3}
-                        >
-                        <h2>{product.name.toUpperCase()}</h2>
-                        
-                        <Product product={product}/>
-                    </Col>
-                ))}
-                </Row>
-            
+            <h1>Products</h1>
+                {loading ? 
+                (<Loader />) :
+                 error ? 
+                ( <Message variant="danger">{error}</Message>)  : 
+                (
+                <Row>
+                    {products && products.length > 0 &&  products.map((product)=>(
+
+                        <Col
+                            key={product._id}
+                            sm={8}
+                            md={6}
+                            lg={4}
+                            xlg={3}
+                            >
+                            {/* <h2>{product.name.toUpperCase()}</h2> */}
+
+                            <Product product={product}/>
+                        </Col>
+                    ))}
+                    </Row>)
+                }
         </>
     )
 }
